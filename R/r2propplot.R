983e39a38c5dfe677a645a44e46f361e525e2d72
ryanreis333 <ryanreis333@gmail.com>
2024-08-12 08:40:48 -0500

    fix

--- R/r2propplot.R
+++ R/r2propplot.R
@@ -9,28 +9,22 @@
 #'
 #' @examples r2propplot()
 #'
-r2propplot <- function(seurat_object = "so", celltype = "celltype", orig.ident = "orig.ident") {
+r2propplot2 <- function(seurat_object, celltype, orig.ident) {
 
+  # Extract metadata from the Seurat object
   meta <- seurat_object[[]]
-  meta <- meta[,c(celltype,orig.ident)]
 
+  # Select relevant columns based on the arguments
+  meta <- meta[, c(celltype, orig.ident)]
+
+  # Group, summarize, and calculate proportions
   prop_data <- meta %>%
-    group_by(orig.ident, celltype) %>%
+    group_by(.data[[orig.ident]], .data[[celltype]]) %>%
     summarise(count = n()) %>%
     mutate(proportion = count / sum(count) * 100)
 
-  ggplot(prop_data, aes(x = orig.ident, y = proportion, fill = celltype)) +
+  # Plot the data using ggplot2
+  ggplot(prop_data, aes(x = .data[[orig.ident]], y = proportion, fill = .data[[celltype]])) +
     geom_bar(stat = "identity") +
-    labs(y = "Proportion (%)", x = "", fill = "Cell Type") +
-    ggtitle("") +
-    theme_minimal() +
-    theme(
-      plot.title = element_text(size = 20, face = "bold"),      # Title
-      axis.title.x = element_text(size = 16),                  # X-axis label
-      axis.title.y = element_text(size = 20),                  # Y-axis label
-      axis.text.x = element_text(size = 18),                   # X-axis text
-      axis.text.y = element_text(size = 18),                   # Y-axis text
-      legend.title = element_text(size = 16),                  # Legend title
-      legend.text = element_text(size = 14)                    # Legend text
-    ) + NoLegend()
+    labs(y = "Proportion (%)", x = "", fill = "Cell Type")
 }
