b9af4b76a7d2b20cc80581505ba8e3bde552ecf5
ryanreis333 <ryanreis333@gmail.com>
2025-05-15 13:54:39 -0500

    updates to functions

--- R/r2prop_plot.R
+++ R/r2prop_plot.R
@@ -4,7 +4,7 @@
 #'
 #' @param seurat_object A Seurat object containing the data to be plotted. The object should include metadata columns corresponding to the cell types and sample identifiers.
 #' @param celltype A string specifying the column name in the Seurat object's metadata that contains the cell type or other categorical metadata to be plotted. By default, it uses "celltype" which refers to the identities of the cells.
-#' @param orig.ident A string specifying the column name in the Seurat object's metadata that contains the sample identifiers or conditions by which the data should be split. The default is "orig.ident".
+#' @param group.by A string specifying the column name in the Seurat object's metadata that contains the sample identifiers or conditions by which the data should be split. The default is "orig.ident".
 #' @param reorder A logical value indicating whether to reorder the samples based on the proportion of the most dominant cell type. If `TRUE`, samples will be reordered; if `FALSE` (default), the original order will be used.
 #'
 #' @return A ggplot object that visualizes the proportion of each cell type or specified metadata category across the samples or conditions. The plot is a bar plot where the x-axis represents the sample identifiers, the y-axis represents the proportion of each category, and the bars are colored by cell type or metadata category.
@@ -15,46 +15,46 @@
 #' r2prop_plot(seurat_object = seurat_obj)
 #'
 #' # Example with specified metadata columns
-#' r2prop_plot(seurat_object = seurat_obj, celltype = "cell_type", orig.ident = "sample_name")
+#' r2prop_plot(seurat_object = seurat_obj, celltype = "cell_type", group.by = "sample_name")
 #'
 #' # Example with different metadata
-#' r2prop_plot(seurat_object = seurat_obj, celltype = "cell_type", orig.ident = "condition")
+#' r2prop_plot(seurat_object = seurat_obj, celltype = "cell_type", group.by = "condition")
 #'
 #' # Example with reordered samples
-#' r2prop_plot(seurat_object = seurat_obj, celltype = "cell_type", orig.ident = "sample_name", reorder = TRUE)
+#' r2prop_plot(seurat_object = seurat_obj, celltype = "cell_type", group.by = "sample_name", reorder = TRUE)
 #'
 #' @import ggplot2
 #' @import dplyr
 
-r2prop_plot <- function(seurat_object, celltype = "celltype", orig.ident = "orig.ident", reorder = FALSE) {
+r2prop_plot <- function(seurat_object, celltype = "celltype", group.by = "orig.ident", reorder = FALSE) {
 
   # Extract metadata from the Seurat object
   meta <- seurat_object[[]]
 
   # Select relevant columns based on the arguments
-  meta <- meta[, c(celltype, orig.ident)]
+  meta <- meta[, c(celltype, group.by)]
 
-  # Group, summarize, and calculate proportions within each sample (orig.ident)
+  # Group, summarize, and calculate proportions within each sample (group.by)
   prop_data <- meta %>%
-    group_by(.data[[orig.ident]], .data[[celltype]]) %>%
+    group_by(.data[[group.by]], .data[[celltype]]) %>%
     summarise(count = n(), .groups = 'drop') %>%
-    group_by(.data[[orig.ident]]) %>%
+    group_by(.data[[group.by]]) %>%
     mutate(proportion = count / sum(count) * 100)
 
-  # Conditionally reorder orig.ident based on the proportion of the most dominant celltype
+  # Conditionally reorder group.by based on the proportion of the most dominant celltype
   if (reorder) {
     dominant_order <- prop_data %>%
-      group_by(.data[[orig.ident]]) %>%
+      group_by(.data[[group.by]]) %>%
       summarise(max_proportion = max(proportion)) %>%
       arrange(desc(max_proportion)) %>%
-      pull(.data[[orig.ident]])
+      pull(.data[[group.by]])
 
-    # Use the reordered orig.ident in the plot
-    plot <- ggplot(prop_data, aes(x = factor(.data[[orig.ident]], levels = dominant_order), y = proportion, fill = .data[[celltype]])) +
+    # Use the reordered group.by in the plot
+    plot <- ggplot(prop_data, aes(x = factor(.data[[group.by]], levels = dominant_order), y = proportion, fill = .data[[celltype]])) +
       geom_bar(stat = "identity")
   } else {
     # Leave the order as is if reorder is set to FALSE
-    plot <- ggplot(prop_data, aes(x = .data[[orig.ident]], y = proportion, fill = .data[[celltype]])) +
+    plot <- ggplot(prop_data, aes(x = .data[[group.by]], y = proportion, fill = .data[[celltype]])) +
       geom_bar(stat = "identity")
   }
 
